1c7a34b40c541312ae3429010173b537ad6a7536
Angeline399 <angelinecalona399@gmail.com>
2024-11-21 14:14:27 +0800

    config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'Employees');
+define('DB_USERNAME', 'angeline');
+define('DB_PASSWORD', 'calona');
+define('DB_NAME', 'demo');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
